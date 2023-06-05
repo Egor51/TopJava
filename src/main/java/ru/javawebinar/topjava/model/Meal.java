92bf6b9dbdae869190d3f0e4efb134950de244f3
Egor51 <kubrakov_egor87@mail.ru>
2023-06-05 20:22:17 +0300

    add id and constructor

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -5,11 +5,20 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 public class Meal {
+    private Integer id;
+    
     private final LocalDateTime dateTime;
 
     private final String description;
 
     private final int calories;
+
+    public Meal(Integer id, LocalDateTime dateTime, String description, int calories) {
+        this.id = id;
+        this.dateTime = dateTime;
+        this.description = description;
+        this.calories = calories;
+    }
 
     public Meal(LocalDateTime dateTime, String description, int calories) {
         this.dateTime = dateTime;
@@ -17,6 +26,12 @@ public class Meal {
         this.calories = calories;
     }
 
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public Integer getId() {
+        return id;
+    }
     public LocalDateTime getDateTime() {
         return dateTime;
     }
