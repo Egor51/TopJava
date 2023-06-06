b75a37d29979e5aee621938afced840f1d8e1df7
Egor51 <kubrakov_egor87@mail.ru>
2023-06-06 16:13:36 +0300

    create init and javadoc

--- src/main/java/ru/javawebinar/topjava/web/MealsServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealsServlet.java
@@ -1,5 +1,7 @@
 package ru.javawebinar.topjava.web;
 
+
+import org.codehaus.jackson.map.ObjectMapper;
 import org.slf4j.Logger;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.model.MealTo;
@@ -13,7 +15,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
+import java.io.PrintWriter;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
@@ -24,42 +26,98 @@ import static org.slf4j.LoggerFactory.getLogger;
 
 public class MealsServlet extends HttpServlet {
     private static final Logger log = getLogger(MealsServlet.class);
-    private final MealRepository mealRepository;
+    private MealRepository mealRepository;
 
-    public MealsServlet() {
+    @Override
+    public void init() {
         this.mealRepository = new MealRepositoryImpl();
     }
 
-    /*
-    Handles HTTP GET requests. Calls displayMeals method to display the list of meals.
-    */
+    /**
+     * Handles HTTP GET requests and displays meals or retrieves information about a specific meal based on its ID.
+     *
+     * @param req  HttpServletRequest
+     * @param resp HttpServletResponse
+     * @throws ServletException
+     * @throws IOException
+     */
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
-        log.debug("redirect to meals");
-        displayMeals(req, resp);
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String mealIdStr = req.getParameter("id");
+
+        if (mealIdStr == null || mealIdStr.trim().isEmpty()) {
+            log.debug("Redirect to meals");
+            displayMeals(req, resp);
+            return;
+        }
+
+        try {
+            int mealId = Integer.parseInt(mealIdStr);
+            Meal meal = mealRepository.getById(mealId);
+
+            if (meal == null) {
+                log.error("Meal with ID " + mealId + " does not exist");
+                resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+                return;
+            }
+
+            resp.setContentType("application/json");
+            resp.setCharacterEncoding("UTF-8");
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            String mealJson = objectMapper.writeValueAsString(meal);
+
+            PrintWriter out = resp.getWriter();
+            out.print(mealJson);
+            out.flush();
+        } catch (NumberFormatException e) {
+            log.error("Failed to parse meal ID", e);
+            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        }
     }
 
-    /*
-        Handles HTTP POST requests. Responsible for creating or updating a meal based on request parameters.
-    */
+    /**
+     * Retrieves all meals, filters them and forwards to JSP for display.
+     *
+     * @param req  HttpServletRequest
+     * @param resp HttpServletResponse
+     * @throws ServletException
+     * @throws IOException
+     */
+    private void displayMeals(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        final int caloriesPerDay = 2000;
+        List<Meal> meals = mealRepository.getAll();
+        List<MealTo> mealsTo = MealsUtil.filteredByStreams(meals, LocalTime.MIN, LocalTime.MAX, caloriesPerDay);
+        req.setAttribute("meals", mealsTo);
+        req.getRequestDispatcher("meals.jsp").forward(req, resp);
+    }
+
+    /**
+     * Handles HTTP POST requests. Creates or updates a meal.
+     *
+     * @param req  HttpServletRequest
+     * @param resp HttpServletResponse
+     * @throws ServletException
+     * @throws IOException
+     */
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
-        try {
-            req.setCharacterEncoding("UTF-8");
-        } catch (UnsupportedEncodingException e) {
-            log.error(e.getMessage(), e);
-        }
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("UTF-8");
+
         if (!isValid(req, "dateTime", "description", "calories")) {
             log.error("Required parameters are null or empty");
             resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
             return;
         }
+
         String mealIdStr = req.getParameter("id");
         String dateTimeStr = req.getParameter("dateTime");
         String description = req.getParameter("description");
         String caloriesStr = req.getParameter("calories");
-        LocalDateTime dateTime = null;
-        int calories = 0;
+
+        LocalDateTime dateTime;
+        int calories;
+
         try {
             dateTime = LocalDateTime.parse(dateTimeStr, DateTimeFormatter.ISO_LOCAL_DATE_TIME);
             calories = Integer.parseInt(caloriesStr);
@@ -68,59 +126,37 @@ public class MealsServlet extends HttpServlet {
             resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
             return;
         }
+
         processMeal(mealIdStr, dateTime, description, calories);
-        try {
-            resp.sendRedirect(req.getContextPath() + "/meals");
-        } catch (IOException e) {
-            log.error(e.getMessage(), e);
-        }
+        resp.sendRedirect(req.getContextPath() + "/meals");
     }
 
-    /*
-        Handles HTTP DELETE requests. Deletes a meal with a given id.
-    */
-    @Override
-    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) {
-        try {
-            Integer id = Integer.parseInt(req.getParameter("id"));
-            mealRepository.deleteById(id);
-        } catch (NumberFormatException e) {
-            log.error("Failed to parse id", e);
-        }
-    }
-
-    /*
-        Gathers all meals from the repository, filters them, sets the filtered meals as a request attribute and forwards the request to the JSP page for display.
-    */
-    private void displayMeals(HttpServletRequest req, HttpServletResponse resp) {
-        final int caloriesPerDay = 2000;
-        try {
-            List<Meal> meals = mealRepository.getAll();
-            List<MealTo> mealsTo = MealsUtil.filteredByStreams(meals, LocalTime.of(7, 0), LocalTime.of(23, 0), caloriesPerDay);
-            req.setAttribute("meals", mealsTo);
-            req.getRequestDispatcher("meals.jsp").forward(req, resp);
-        } catch (ServletException | IOException e) {
-            log.error(e.getMessage(), e);
-        }
-    }
-
-    /*
-        Helper method to process a meal. If an id is provided, updates the corresponding meal. If no id is provided, creates a new meal.
-    */
+    /**
+     * Processes a meal. Updates if an id is provided, creates a new one otherwise.
+     *
+     * @param mealIdStr   meal id as string
+     * @param dateTime    meal date and time
+     * @param description meal description
+     * @param calories    meal calories
+     */
     private void processMeal(String mealIdStr, LocalDateTime dateTime, String description, int calories) {
         if (mealIdStr != null && !mealIdStr.trim().isEmpty()) {
             Meal updatedMeal = new Meal(Integer.parseInt(mealIdStr), dateTime, description, calories);
-            mealRepository.updateById(updatedMeal);
+            mealRepository.update(updatedMeal);
         } else {
             Meal meal = new Meal(dateTime, description, calories);
-            mealRepository.save(meal);
+            mealRepository.create(meal);
             log.debug("create meal");
         }
     }
 
-    /*
-        Helper method to check if all the specified request parameters are valid (not null and not empty).
-    */
+    /**
+     * Checks if all the specified request parameters are valid (not null and not empty).
+     *
+     * @param req    HttpServletRequest
+     * @param params parameters to be checked
+     * @return boolean true if all parameters are valid, false otherwise
+     */
     private boolean isValid(HttpServletRequest req, String... params) {
         for (String param : params) {
             String value = req.getParameter(param);
@@ -130,4 +166,19 @@ public class MealsServlet extends HttpServlet {
         }
         return true;
     }
+
+    /**
+     * Handles HTTP DELETE requests. Deletes a meal.
+     *
+     * @param req  HttpServletRequest
+     * @param resp HttpServletResponse
+     * @throws ServletException
+     * @throws IOException
+     */
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Integer id = Integer.parseInt(req.getParameter("id"));
+        mealRepository.deleteById(id);
+    }
 }
+
