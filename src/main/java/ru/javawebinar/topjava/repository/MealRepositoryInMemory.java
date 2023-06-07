4f42718253d42ed2a6b72f298a25d220525e2f14
Egor51 <kubrakov_egor87@mail.ru>
2023-06-07 11:52:07 +0300

    rename class and fix update

--- src/main/java/ru/javawebinar/topjava/repository/MealRepositoryInMemory.java
+++ src/main/java/ru/javawebinar/topjava/repository/MealRepositoryInMemory.java
@@ -1,19 +1,16 @@
 package ru.javawebinar.topjava.repository;
 
-import org.slf4j.Logger;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.util.MealsUtil;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
-import static org.slf4j.LoggerFactory.getLogger;
 
-public class MealRepositoryImpl implements MealRepository {
-    private static final Logger log = getLogger(MealRepositoryImpl.class);
+public class MealRepositoryInMemory implements MealRepository {
     private final Map<Integer, Meal> meals = new ConcurrentHashMap<>();
-    private final AtomicInteger idGenerator = new AtomicInteger(0);
+    private final AtomicInteger idGenerator = new AtomicInteger(1);
 
-    public MealRepositoryImpl() {
+    public MealRepositoryInMemory() {
         List<Meal> initialMeals = MealsUtil.getMeals();
         initialMeals.forEach(this::create);
     }
@@ -32,10 +29,8 @@ public class MealRepositoryImpl implements MealRepository {
     }
 
     @Override
-    public synchronized Meal update(Meal meal) {
-        if (meals.containsKey(meal.getId())) {
-            meals.put(meal.getId(), meal);
-            log.debug("Meal with id " + meal.getId() + " updated");
+    public Meal update(Meal meal) {
+        if (meals.replace(meal.getId(), meal) != null) {
             return meal;
         }
         return null;
@@ -44,7 +39,6 @@ public class MealRepositoryImpl implements MealRepository {
     @Override
     public void deleteById(int id) {
         meals.remove(id);
-        log.debug("Meal with id " + id + " removed");
     }
 
     @Override
