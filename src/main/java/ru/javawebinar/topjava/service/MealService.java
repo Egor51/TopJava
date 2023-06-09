bbb3318074a6b97d02abe680d1ef344bfc75fd93
Egor51 <kubrakov_egor87@mail.ru>
2023-06-09 15:14:59 +0300

    creat class

--- src/main/java/ru/javawebinar/topjava/service/MealService.java
+++ src/main/java/ru/javawebinar/topjava/service/MealService.java
@@ -1,9 +1,42 @@
 package ru.javawebinar.topjava.service;
 
+import org.springframework.stereotype.Service;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 
-public class MealService {
+import java.util.Collection;
 
-    private MealRepository repository;
+import static ru.javawebinar.topjava.util.ValidationUtil.checkNotFoundWithId;
+import static ru.javawebinar.topjava.web.SecurityUtil.authUserId;
+
+@Service
+public class MealService {
+    private final MealRepository repository;
+
+    public MealService(MealRepository repository) {
+        this.repository = repository;
+    }
+
+
+    public Collection<Meal> getAll() {
+        return repository.getAll(authUserId());
+    }
+
+    public Meal create(Meal meal) {
+        return (Meal) repository.save(meal, authUserId());
+    }
+
+    public void delete(int id) {
+        checkNotFoundWithId(repository.delete(id, authUserId()), id);
+    }
+
+    public Meal get(int id) {
+        return checkNotFoundWithId(repository.get(id, authUserId()), id);
+    }
+
+    public void update(Meal meal) {
+        checkNotFoundWithId(meal,meal.getId());
+        repository.save(meal, authUserId());
+    }
 
 }
