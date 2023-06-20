9ef1d251a0e9ee8b14ced77c5a265871f8ed41a3
Egor51 <kubrakov_egor87@mail.ru>
2023-06-20 14:33:21 +0300

    fix asserMach methods

--- src/test/java/ru/javawebinar/topjava/AssertMatch.java
+++ src/test/java/ru/javawebinar/topjava/AssertMatch.java
@@ -1,20 +1,27 @@
 package ru.javawebinar.topjava;
 
+import ru.javawebinar.topjava.model.AbstractBaseEntity;
+
 import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AssertMatch {
+    private static String [] ignoredFields;
 
-    public static <T> void assertMatch(T actual, T expected, String... ignoredFields) {
+    public AssertMatch(String... ignoredFields) {
+        AssertMatch.ignoredFields = ignoredFields;
+    }
+
+    public static <T extends AbstractBaseEntity> void assertMatch(T actual, T expected) {
         assertThat(actual).usingRecursiveComparison().ignoringFields(ignoredFields).isEqualTo(expected);
     }
 
-    public static <T> void assertMatch(Iterable<T> actual, T... expected) {
+    public static <T extends AbstractBaseEntity> void assertMatch(Iterable<T> actual, T... expected) {
         assertMatch(actual, Arrays.asList(expected));
     }
 
-    public static <T> void assertMatch(Iterable<T> actual, Iterable<T> expected, String... ignoredFields) {
+    public static <T extends AbstractBaseEntity> void assertMatch(Iterable<T> actual, Iterable<T> expected) {
         assertThat(actual).usingRecursiveFieldByFieldElementComparatorIgnoringFields(ignoredFields).isEqualTo(expected);
     }
 }
